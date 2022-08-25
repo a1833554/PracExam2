e6c007bbcd8f1035d01ad7bc6499076292ef0174
a1833554 <110015029+a1833554@users.noreply.github.com>
2022-08-25 13:36:23 +0930

    Add files via upload

---
+++ main-2-1.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+extern string month_lookup(int month);
+
+int main(){
+
+cout<<month_lookup(3)<<endl;
+cout<<month_lookup(14)<<endl;
+
+
+
+}
